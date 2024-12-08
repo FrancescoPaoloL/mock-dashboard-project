ff573baf2e13937b9334310256d9d065454b4085
FrancescoPaoloL <francescopaololezza@gmail.com>
2024-12-08 09:21:53 +0100

    Cleaned situation

--- HelloWorld/public/js/app.js
+++
@@ -1,5 +0,0 @@
-// JavaScript code to display an alert when the button is clicked
-document.getElementById('clickMe').addEventListener('click', () => {
-    alert('Button clicked! Welcome to the Node.js website!');
-  });
-  
